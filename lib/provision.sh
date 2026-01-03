1f661c950ff206befbc86a23408dcf81f74f1782
tanilink <rayyan.kontak@gmail.com>
2026-01-03 19:32:08 +0700

    Update provision.sh

--- lib/provision.sh
+++ lib/provision.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo 'Provision placeholder'
+
+# Mendapatkan data toko dan konfigurasi
+source ~/.toko/env
+
+# Fungsi untuk melakukan provisioning server
+provision_server() {
+  echo "Memulai provisioning untuk toko: $TOKO_ID"
+  # Menambahkan setup lainnya di sini, misalnya update paket atau instalasi lainnya
+  apt update -y && apt upgrade -y
+  echo "Provisioning selesai untuk toko: $TOKO_ID"
+}
+
+# Menjalankan provisioning
+provision_server
