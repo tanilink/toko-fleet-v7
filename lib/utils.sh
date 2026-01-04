be7407540a59357c8efa159aea87bd3c6dfcd84c
tanilink <rayyan.kontak@gmail.com>
2026-01-04 19:09:36 +0700

    Update utils.sh

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Fungsi untuk mengirimkan pesan umum
-send_message() {
-  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-    -d chat_id="$CHAT_ID" -d text="$1"
-}
-
-# Fungsi untuk membuat slug (format URL)
 slugify() {
-  echo "$1" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g;s/--*/-/g;s/^-//;s/-$//'
+  echo "$1" | tr '[:upper:]' '[:lower:]' \
+  | sed 's/[^a-z0-9]/-/g;s/--*/-/g;s/^-//;s/-$//'
 }
 
-# Fungsi untuk menampilkan status sistem
-system_status() {
-  echo "Status Sistem: Toko $TOKO_ID"
-  echo "Baterai: $(termux-battery-status | jq -r '.percentage')%"
+send_telegram() {
+  local FILE="$1"
+  curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+    -F chat_id="$CHAT_ID" \
+    -F document=@"$FILE" >/dev/null
 }
